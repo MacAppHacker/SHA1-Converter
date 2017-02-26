312195658583f3b395ed7be2e256cf82af1b181e
amikhaielYoungDev <amikhaiel@protonmail.ch>
2017-02-26 20:57:39 +0100

    Initial Commit

---
+++ SHA1-Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SHA1-Converter
+//
+//  Created by adi on 26.02.17.
+//  Copyright © 2017 adi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
